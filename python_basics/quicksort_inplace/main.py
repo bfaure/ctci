2dec5665525547f4e3f1acb0b34bc6da80ba6938
Brian Faure <bfaure23@gmail.com>
2017-11-14 17:23:13 -0500

    added in benchmark for inplace quicksort

--- python_basics/quicksort_inplace/main.py
+++ python_basics/quicksort_inplace/main.py
@@ -15,11 +15,11 @@ def quicksort(a):
 	return quicksort(smaller)+equal+quicksort(larger)
 
 def partition(a,low,high):
-	i=low-1
-	pivot=a[high]
-	for j in range(low,high):
+	i=low-1 # to be the final pivot index
+	pivot=a[high] # pivot value itself
+	for j in range(low,high): # iterate up to pivot index
 		if a[j]<=pivot:
-			i+=1
+			i+=1 # increment final pivot index 
 			a[i],a[j]=a[j],a[i]
 	a[i+1],a[high]=a[high],a[i+1]
 	return i+1
@@ -32,7 +32,39 @@ def quicksort_inplace(a,low=0,high=None):
 		quicksort_inplace(a,low,p_idx-1)
 		quicksort_inplace(a,p_idx+1,high)
 
-a=create_array()
-print "Unsorted:",a 
-quicksort_inplace(a)
-print "Sorted:  ",a 
+
+from time import time
+times={'initial':[],'inplace':[]}
+sizes=[10,100,1000,10000,100000,1000000]
+samples=3
+
+for s in sizes:
+
+	# regular quicksort
+	tot=0.0
+	for _ in range(samples):
+		a=create_array(size=s,max=s)
+		t0=time()
+		a=quicksort(a)
+		t1=time()
+		tot+=(t1-t0)
+	times['initial'].append(float(tot/samples))
+
+	# inplace quicksort
+	tot=0.0
+	for _ in range(samples):
+		a=create_array(size=s,max=s)
+		t0=time()
+		quicksort(a)
+		t1=time()
+		tot+=(t1-t0)
+	times['inplace'].append(float(tot/samples))
+
+print "n\tQuicksort\tQuicksort (inplace)"
+print "_"*35
+for i,s in enumerate(sizes):
+	print "%d\t%0.5f  \t%0.5f"%(
+		s,
+		times['initial'][i],
+		times['inplace'][i])
+
